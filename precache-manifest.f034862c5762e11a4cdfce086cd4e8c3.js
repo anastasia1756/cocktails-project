9970c4a6c5d20e16b807b57129a32c15bc906c00
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-14 00:16:15 +0200

    Updates

---
+++ precache-manifest.f034862c5762e11a4cdfce086cd4e8c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df17f3b0d67642eae4443038fa4b4d2",
+    "url": "/cocktails-project/index.html"
+  },
+  {
+    "revision": "97e047266268b62cf91e",
+    "url": "/cocktails-project/static/css/main.5c529f91.chunk.css"
+  },
+  {
+    "revision": "e1f6309a6f0d3e350015",
+    "url": "/cocktails-project/static/js/2.72303b8e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cocktails-project/static/js/2.72303b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e047266268b62cf91e",
+    "url": "/cocktails-project/static/js/main.d3a73d05.chunk.js"
+  },
+  {
+    "revision": "38a9fcebb4d9fbb871a8",
+    "url": "/cocktails-project/static/js/runtime-main.d4f9d11b.js"
+  },
+  {
+    "revision": "9a3d26451b294fc390aee4ae147a45c7",
+    "url": "/cocktails-project/static/media/logo.9a3d2645.svg"
+  }
+]);
