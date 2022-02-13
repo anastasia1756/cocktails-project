840d7754ca2acd6cfecee0207c39cd9f4db64386
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-02-13 21:47:37 +0200

    add about page and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+        perspiciatis iure eligendi facilis debitis vitae libero itaque? Nesciunt
+        quas similique quis quaerat minus repudiandae, omnis amet placeat at
+        cumque in soluta necessitatibus veniam. Sapiente, quos consequatur.
+        Temporibus, maiores eius?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
